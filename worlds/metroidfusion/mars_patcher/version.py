218af977cab2cc40defbc50f335ae4f1a575e2d9
Miepee <janbidler00@protonmail.com>
2026-03-01 22:43:55 +0100

    Update to mars patcher 0.12.0

--- worlds/metroidfusion/mars_patcher/version.py
+++ worlds/metroidfusion/mars_patcher/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.11.0'
-__version_tuple__ = version_tuple = (0, 11, 0)
+__version__ = version = '0.12.0'
+__version_tuple__ = version_tuple = (0, 12, 0)
 
-__commit_id__ = commit_id = 'g8d41e0db2'
+__commit_id__ = commit_id = 'ge8bafa1eb'
