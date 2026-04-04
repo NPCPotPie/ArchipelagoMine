02627f4abcdfd4967672229011281d0ad5a0ea4b
npcpotpie <sean.i.fulton@gmail.com>
2026-04-04 05:20:30 -0700

    Add new PONR label to valid item names for use in debugging

--- worlds/metroidfusion/Items.py
+++ worlds/metroidfusion/Items.py
@@ -30,7 +30,7 @@ for item in all_item_data:
 
 item_names = [item for item in item_table.keys()]
 
-valid_item_names = [*item_names, *[event[3] for event in events], "Wall Jump Boots"]
+valid_item_names = [*item_names, *[event[3] for event in events], "Wall Jump Boots", "Point of No Return"]
 
 # Everything not listed here has a quantity of one
 default_item_quantities = {
