d93c3bbeb22a6f0e115106b73d1e97452e882fad
npcpotpie <sean.i.fulton@gmail.com>
2026-04-20 01:30:50 -0700

    Re-wrote concept for ammo requirement checking. Now only checks count of missile tanks and power bomb tanks on the player.

--- worlds/metroidfusion/Logic.py
+++ worlds/metroidfusion/Logic.py
@@ -45,30 +45,29 @@ class LogicObject:
                 if self.options.CombatDifficulty >= self.options.CombatDifficulty.option_expert:
                     energy_tanks_value = energy_tanks_value // 2
             if missile_ammo_value > 0:
-                missile_data_ammo_value: int = self.options.MissileDataAmmo.value
-                missile_tank_ammo_total: int = self.options.MissileTankAmmo * state.count("Missile Tank", self.player)
                 match self.options.CombatDifficulty.value:
                     case self.options.CombatDifficulty.option_beginner:
                         missile_ammo_value = -int(-(missile_ammo_value * 1.25) // 1)
                     case self.options.CombatDifficulty.option_advanced:
                         missile_ammo_value = -int(-(missile_ammo_value * 1.1) // 1)
-                has_enough_missiles = (missile_data_ammo_value + missile_tank_ammo_total) > missile_ammo_value
-            else:
-                has_enough_missiles = True
+                missile_ammo_value -= self.options.MissileDataAmmo.value
+                if self.options.MissileTankAmmo.value > 0:
+                    missile_ammo_value = -int(-(missile_ammo_value / self.options.MissileTankAmmo.value) // 1)
             if power_bomb_ammo_value > 0:
-                power_bomb_data_value: int = self.options.PowerBombDataAmmo.value
-                power_bomb_tank_total: int = self.options.PowerBombTankAmmo * state.count("Power Bomb Tank", self.player)
-                has_enough_power_bomb = (power_bomb_data_value + power_bomb_tank_total) > power_bomb_ammo_value
-            else:
-                has_enough_power_bomb = True
+                power_bomb_ammo_value -= self.options.PowerBombDataAmmo.value
+                if self.options.PowerBombTankAmmo.value > 0:
+                    power_bomb_ammo_value = -int(-(power_bomb_ammo_value / self.options.PowerBombTankAmmo.value) // 1)
             if expression is None:
                 expression = (state.has_all(requirement_list, self.player)
                               and state.has("Energy Tank", self.player, energy_tanks_value)
-                              and has_enough_missiles and has_enough_power_bomb)
+                              and state.has("Missile Tank", self.player, missile_ammo_value)
+                              and state.has("Power Bomb Tank", self.player, power_bomb_ammo_value))
             else:
                 expression = (expression
                               or state.has_all(requirement_list, self.player)
-                              and state.has("Energy Tank", self.player, energy_tanks_value))
+                              and state.has("Energy Tank", self.player, energy_tanks_value)
+                              and state.has("Missile Tank", self.player, missile_ammo_value)
+                              and state.has("Power Bomb Tank", self.player, power_bomb_ammo_value))
         return expression
 
 
