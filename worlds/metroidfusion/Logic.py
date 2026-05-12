eb2250a69e8532317fcab7c06d5580fc605f22ae
npcpotpie <sean.i.fulton@gmail.com>
2026-05-11 18:40:52 -0700

    Update logic subsystem to have flags on each possibility identifying if they are YAML enabled. A disabled YAML setting will substitute a fake requirement in its place to provide the system something to go off of.

--- worlds/metroidfusion/Logic.py
+++ worlds/metroidfusion/Logic.py
@@ -14,6 +14,7 @@ class LogicObject:
     energy_tanks: list[int] = []
     missile_ammo: list[int] = []
     power_bomb_ammo: list[int] = []
+    yaml_enabled: list[bool] = []
     calculated_energy_tanks: int = 0
     player: int
     options: "MetroidFusionOptions"
@@ -73,30 +74,34 @@ class LogicObject:
 def create_logic_rule_for_list(
         requirements: list[Requirement],
         options: "MetroidFusionOptions",
-        debug: bool = False) -> tuple[list[set[str]], list[int], list[int], list[int]]:
+        debug: bool = False) -> tuple[list[set[str]], list[int], list[int], list[int], list[bool]]:
     if debug:
         print("Create logic rule for list...")
         logging.info("Create logic rule for list...")
-    r_e_m_p: tuple[list[set[str]], list[int], list[int], list[int]] = ([], [], [], [])
+    r_e_m_p: tuple[list[set[str]], list[int], list[int], list[int], list[bool]] = ([], [], [], [], [])
     for requirement in requirements:
         for (new_rule,
              energy_tanks_in_rule,
              missile_ammo_in_rule,
-             power_bomb_ammo_in_rule) in create_logic_rule(requirement, options, debug):
+             power_bomb_ammo_in_rule,
+             yaml_enabled) in create_logic_rule(requirement, options, debug):
             r_e_m_p[0].append(new_rule)
             r_e_m_p[1].append(energy_tanks_in_rule)
             r_e_m_p[2].append(missile_ammo_in_rule)
             r_e_m_p[3].append(power_bomb_ammo_in_rule)
+            r_e_m_p[4].append(yaml_enabled)
     if debug:
         for (requirement,
              energy_tanks,
              missiles,
-             power_bombs) in zip(*r_e_m_p):
+             power_bombs,
+             yaml_enabled) in zip(*r_e_m_p):
             debug_string = ("Logic rule:"
                             f"\nRequirements: {requirement}"
                             f"\nEnergy Tanks: {energy_tanks}"
                             f"\nMissiles: {missiles}"
                             f"\nPower Bombs: {power_bombs}"
+                            f"\nEnabled: {yaml_enabled}"
                             "\n===\n")
             print(debug_string)
             logging.info(debug_string)
@@ -105,9 +110,9 @@ def create_logic_rule_for_list(
 def create_logic_rule(
         requirement: Requirement,
         options: "MetroidFusionOptions",
-        debug: bool = False) -> list[tuple[set[str], int, int, int]]:
+        debug: bool = False) -> list[tuple[set[str], int, int, int, bool]]:
     if requirement.check_option_enabled(options):
-        possibilities: list[tuple[set[str], int, int, int]] = []
+        possibilities: list[tuple[set[str], int, int, int, bool]] = []
         hard_items: set[str] = set()
         unpack_requirement(
             requirement,
@@ -124,8 +129,9 @@ def create_logic_rule(
                 (f"\t({requirements_list},\n"
                  f"\t\tEnergy Tanks: {energy_tanks},\n"
                  f"\t\tMissiles: {missiles},\n"
-                 f"\t\tPower Bombs: {power_bombs})")
-                for requirements_list, energy_tanks, missiles, power_bombs in possibilities
+                 f"\t\tPower Bombs: {power_bombs})"
+                 f"\t\tEnabled: {yaml_enabled}")
+                for requirements_list, energy_tanks, missiles, power_bombs, yaml_enabled in possibilities
             ]
             debug_string = ("Create logic rule...\n"
                             f"Requirement: {requirement.name}\n"
@@ -141,14 +147,14 @@ def create_logic_rule(
 
 def unpack_requirement(
         requirement: Requirement,
-        possibilities: list[tuple[set[str], int, int, int]],
+        possibilities: list[tuple[set[str], int, int, int, bool]],
         parent_items: set[str],
         options: "MetroidFusionOptions",
         parent_hard_items: set[str],
         parent_energy_tanks: int = 0,
         parent_missile_ammo: int = 0,
         parent_power_bomb_ammo: int = 0,
-        debug = False) -> list[tuple[set[str], int, int, int]]:
+        debug = False) -> list[tuple[set[str], int, int, int, bool]]:
     """Unpacks a requirement into a list of possible item sets each paired with an integer of energy tanks"""
     if debug:
         logging.info(f"Requirement {requirement.name}. "
@@ -180,6 +186,8 @@ def unpack_requirement(
                 # Check if all requirements in permutation have enabled options
                 if not all([nested_requirement.check_option_enabled(options)
                         for nested_requirement in requirements_permutation]):
+                    # If one is disabled, add a possibility with a flag telling that it's false.
+                    possibilities.append(( {"False Requirement Flag"}, 0, 0, 0, False ))
                     if debug:
                         print(f"Permutation disabled due to options: ["
                               f"{", ".join(nested_requirement.name 
@@ -193,7 +201,7 @@ def unpack_requirement(
                 current_parent_items = parent_items.copy()
                 parent_hard_items |= requirement.hard_items_needed
                 parent_items |= requirement.items_needed
-                new_possibilities: list[tuple[set[str], int, int, int]] = []
+                new_possibilities: list[tuple[set[str], int, int, int, bool]] = []
                 for nested_requirement in requirements_permutation:
                     and_possibilities = unpack_requirement(
                         nested_requirement,
@@ -211,18 +219,19 @@ def unpack_requirement(
                         new_possibilities = [(p[0][0] | p[1][0],
                                               max(p[0][1], p[1][1]),
                                               p[0][2] + p[1][2],
-                                              p[0][3] + p[1][3])
+                                              p[0][3] + p[1][3],
+                                              True)
                                              for p in itertools_product(current_new_possibilities, and_possibilities)]
                     elif not new_possibilities:
                         new_possibilities.extend(and_possibilities)
-                for (n_r_items, n_r_energy, n_r_missiles, n_r_pbs) in new_possibilities:
+                for (n_r_items, n_r_energy, n_r_missiles, n_r_pbs, n_r_yaml) in new_possibilities:
                     combined_items = n_r_items | requirement.items_needed
                     calculated_energy = max(n_r_energy, requirement.energy_tanks_needed)
                     hard_test: bool = parent_hard_items.issubset(combined_items)
                     possibility_exists_test: bool = \
                         (combined_items, calculated_energy, n_r_missiles, n_r_pbs) in possibilities
                     if hard_test and not possibility_exists_test:
-                        possibilities.append( (combined_items, calculated_energy, n_r_missiles, n_r_pbs) )
+                        possibilities.append( (combined_items, calculated_energy, n_r_missiles, n_r_pbs, n_r_yaml) )
                     elif debug:
                         print(f"Skipping Possibility: {combined_items}")
                         logging.info(f"Skipping Possibility: {combined_items}")
@@ -239,9 +248,11 @@ def unpack_requirement(
             possibilities.append(( parent_items | requirement.items_needed,
                                    max(parent_energy_tanks, requirement.energy_tanks_needed),
                                    parent_missile_ammo + requirement.missile_ammo_needed,
-                                   parent_power_bomb_ammo + requirement.power_bomb_ammo_needed ))
+                                   parent_power_bomb_ammo + requirement.power_bomb_ammo_needed,
+                                   True))
     else:
         print(f"Requirement {requirement.name} disabled due to options.")
         logging.info(f"Requirement {requirement.name} disabled due to options.")
-        return []
+        possibilities.append(( {"False Requirement Flag"}, 0, 0, 0, False ))
+        return possibilities
     return possibilities
